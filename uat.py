5afec71cc5b73bd1928f06796ea43fe4242e290c
Shubham Bhoilkar <91750972+shubhambhoilkar@users.noreply.github.com>
2023-11-20 15:26:59 +0530

    Update and rename uat to uat.py

---
+++ uat.py
@@ -0,0 +1,7 @@
+#UAT is use to perfome initial tasks and actions before implementation.
+dict_core ={"Day0":[0,1,0,1],
+           "Day1":[1,1,1,0],
+           "Day2":[1,0,0,0],
+           "Day3":[0,0,0,0],
+           "Day4":[0,0,0,1],
+           "Day5":[1,1,1,1],}
